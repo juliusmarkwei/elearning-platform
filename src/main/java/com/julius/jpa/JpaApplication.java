705befabcb4197b701c1f036c3191735a2f9068d
juliusmarkwei <juliusmarkwei2000@gmail.com>
2024-09-10 02:35:44 +0000

    queries, modifying, & transactional

--- src/main/java/com/julius/jpa/JpaApplication.java
+++ src/main/java/com/julius/jpa/JpaApplication.java
@@ -20,19 +20,35 @@ public class JpaApplication {
     public CommandLineRunner commandLineRunner(AuthorRepository authorRepository, VideoRepository videoRepository) {
 
         return args -> {
-            /* for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 50; i++) {
                 Faker faker = new Faker();
 
                 var author = Author.builder()
                         .firstName(faker.name().firstName())
                         .lastName(faker.name().lastName())
                         .age(faker.number().numberBetween(19, 50))
-                        .email("contact" + i+1 + "@gmail.com")
+                        .email("contact" + i + 1 + "@gmail.com")
                         .build();
                 authorRepository.save(author);
             }
 
-           var video = Video.builder()
+            //update author with ID = 1
+            var author = Author.builder()
+                    .id(1)
+                    .firstName("Julius")
+                    .lastName("Markwei")
+                    .age(24)
+                    .email("juliusmarkwei@gmail.com")
+                    .build();
+            authorRepository.save(author);
+
+            //update Author a set a.age = 22 where a.id = 1
+//            authorRepository.updateAuthor(22, 1);
+
+            //update Author a set a.age = :age
+            authorRepository.updateAllAuthorsAges(99);
+
+           /*var video = Video.builder()
                     .name("abc")
                     .length(5)
                     .build();
--- src/main/java/com/julius/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/julius/jpa/repositories/AuthorRepository.java
@@ -1,11 +1,25 @@
 package com.julius.jpa.repositories;
 
 import com.julius.jpa.models.Author;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    void updateAuthor(Integer age, Integer id);
+
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    void updateAllAuthorsAges(Integer age);
+
+
     //select * from author where first_name = 'julius'
     List<Author> findAllByFirstName(String firstName);
 
